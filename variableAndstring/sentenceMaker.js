9a421773976ff8bac8a2426ca5d1cf7bafe05d98
janajayamani <janajayamani@gmail.com>
2025-10-17 19:29:01 +0530

    short sentences file added

---
+++ variableAndstring/sentenceMaker.js
@@ -0,0 +1,27 @@
+let adjective, noun, verb, place, adjective2, noun2;
+
+adjective = "boy";
+noun = "joseph";
+noun2 = "pineapple";
+place = "graza";
+adjective2 = "a big farm";
+verb = "happened yesterday";
+
+let firstStory;
+
+firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+"."+" The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("First story: "+firstStory);
+
+adjective = "girl";
+noun = "quince";
+verb = "happening now";
+place = "monto carlo";
+adjective2 = "a quiet home";
+noun2 = "strawberry";
+
+let secondStory;
+
+secondStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+"."+" The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("Second story: "+secondStory);
