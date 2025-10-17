c82e7efecafa4f5b13946fc8404b2f62eb334f09
janajayamani <janajayamani@gmail.com>
2025-10-17 18:58:21 +0530

    new Trivia file added

---
+++ buildingTrivia.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName, botLocation, favoriteLanguage;
+botName = "Jana";
+botLocation = "Chennai";
+favoriteLanguage = "JavaScript";
+
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+
+console.log("My favorite programming language is "+favoriteLanguage+".");
+
+let codingFact = "I always feel i am best problem solver, while coding in "+ favoriteLanguage+".";
+console.log(codingFact)
+// reassign codingFact with new fact
+codingFact = "I am the best creator using "+favoriteLanguage+".";
+console.log(codingFact)
+
+// Again reassign codingFact with new fact
+codingFact = "I'm best to do in "+favoriteLanguage+".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - Jana from Chennai.");
